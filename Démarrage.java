2f7d3d873fa7ce3e1f45e49626ddeff3e5531079
Neoshark80 <61883742+Neoshark80@users.noreply.github.com>
2020-03-13 15:12:08 +0100

    Add files via upload

--- Démarrage.java
+++ Démarrage.java
@@ -10,29 +10,34 @@ public class Démarrage {
 	private static void jeuNiOuiNiNon() {
 		listeQuestion listeQuestion = new listeQuestion();
 		int nombreReponseBonne = 0;
-		String reponse = new String();
-		int index = (int) (Math.random() * 10);
+		String reponse = new String(), play = "oui";
 		Scanner sc = new Scanner(System.in);
+		Scanner sc1 = new Scanner(System.in);
 		System.out.println("Ni oui, Ni non\n\n");
-		while (!reponse.equals("Oui") && !reponse.equals("oui") && !reponse.equals("non") && !reponse.equals("Non")) {
-			if (nombreReponseBonne == 0) {
-				nombreReponseBonne += 1;
-				System.out.println(
-						"Le but du jeu est de répondre aux questions posées, sans écrire les mots \"oui\" ou \"non\"\n");
-				System.out.println("Question n°" + nombreReponseBonne + "\n");
-				System.out.println(listeQuestion.get());
-				reponse = sc.nextLine();
-			} else {
-				nombreReponseBonne += 1;
-				System.out.println();
-				System.out.println("Question n°" + nombreReponseBonne + "\n");
-				System.out.println(listeQuestion.get());
-				reponse = sc.nextLine();
+		while (play.equals("Oui") || play.equals("oui")) {
+			while (!reponse.equals("Oui") && !reponse.equals("oui") && !reponse.equals("non")
+					&& !reponse.equals("Non")) {
+				if (nombreReponseBonne == 0) {
+					nombreReponseBonne += 1;
+					System.out.println(
+							"Le but du jeu est de répondre aux questions posées, sans écrire les mots \"oui\" ou \"non\"\n");
+					System.out.println("Question n°" + nombreReponseBonne + "\n");
+					System.out.println(listeQuestion.get());
+					reponse = sc.nextLine();
+				} else {
+					nombreReponseBonne += 1;
+					System.out.println();
+					System.out.println("Question n°" + nombreReponseBonne + "\n");
+					System.out.println(listeQuestion.get());
+					reponse = sc.nextLine();
+				}
 			}
+			System.out.println("Perdu, vous avez répondu: \"" + reponse + "\" à la question.\n Votre score est de "
+					+ nombreReponseBonne + " points.");
+			reponse = "autre";
+			nombreReponseBonne = 0;
+			System.out.println("Voulez vous rejouer ?");
+			play = sc1.nextLine();
 		}
-		System.out.println("Perdu, vous avez répondu: \"" + reponse + "\" à la question.\n Votre score est de "
-				+ nombreReponseBonne + " points.");
-		reponse = null;
-		nombreReponseBonne = 0;
 	}
 }
